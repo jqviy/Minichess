69319af3f1dedde9724cce251eec449eef433aa3
alejandroserr <alejandro.serranov@alumnos.upm.es>
2024-05-01 09:44:10 +0200

    Quitar movimientos de pieza.cpp

--- src/Pieza.cpp
+++ src/Pieza.cpp
@@ -17,16 +17,3 @@ Pieza::Pieza(TIPO t, COLOR c, int x, int y)
 
 
 
-bool Pieza::mLineal(Pieza inicio, Pieza fin)
-{
-	if ((fin.fila != inicio.fila && fin.columna == inicio.columna) || (fin.columna != inicio.columna && fin.fila == inicio.fila)) return true;
-	return false;
-}
-
-
-bool Pieza::mDiagonal(Pieza inicio, Pieza fin)
-{
-	if (((abs(fin.fila - inicio.fila) + abs(fin.columna - fin.columna)) % 2 == 0) && (abs(fin.fila - inicio.fila) == abs(fin.columna - inicio.columna))) return true;
-	return false;
-}
-
